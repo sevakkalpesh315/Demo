8d188c6921bec133d46ca142eeff32e89f1d44cc
sevakkalpesh315 <sevakkalpesh@gmail.com>
2016-06-03 15:08:30 +0100

    Added Betterpickers

---
+++ app/src/main/java/chintan/khetiya/sqlite/cursor/My_Blog.java
@@ -0,0 +1,18 @@
+package chintan.khetiya.sqlite.cursor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class My_Blog extends Activity {
+    private WebView webView;
+
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.my_blog);
+
+	
+
+    }
+
+}
